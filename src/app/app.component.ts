6689383e54297e4fd82bce973be7aa730f841101
Ahmed Khalil <ahmed8157@gmail.com>
2019-11-22 10:28:08 +0200

    add support for arabic language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,27 @@ import { TranslateService } from "@ngx-translate/core";
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
+  lang;
+  ltrrtl;
   constructor(public translate: TranslateService) {
     translate.addLangs(['en', 'ar']);
     translate.setDefaultLang('en');
-
     const browserLang = translate.getBrowserLang();
     translate.use(browserLang.match(/en|ar/) ? browserLang : 'en');
+    this.translate.onLangChange.subscribe((event) => {
+      this.lang =event.lang;
+      if (event.lang == 'ar') {
+      this.ltrrtl = 'rtl';
+      }
+      else {
+      this.ltrrtl = 'ltr';
+      }
+      document.getElementsByTagName("html")[0].setAttribute('lang', this.lang);
+      document.getElementsByTagName("body")[0].setAttribute('dir', this.ltrrtl);
+      });
   }
   useLanguage(language: string) {
     this.translate.use(language);
-}
+  }
+  
 }
