aaf48533d5c6f23671a6d03056b8a88affb3a415
Ahmed Khalil <ahmed8157@gmail.com>
2019-11-29 08:12:06 +0200

    create the products array

---
+++ src/app/Products.ts
@@ -0,0 +1,80 @@
+export const Products = [
+    {
+        Name: "Product-1",
+        Brand: "Brand-1",
+        Images: ["https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder",],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur laoreet fermentum diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce congue leo in dolor tempus, a accumsan felis commodo. Fusce id ultrices tellus, sed pretium ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+        featured: "yes",
+        "Technical-Specifications": { Size: "16 mm", Pressure: "Low Pressure" }
+    },    {
+        Name: "Product-2",
+        Brand: "Al Fanar",
+        Images: ["https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder",],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur laoreet fermentum diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce congue leo in dolor tempus, a accumsan felis commodo. Fusce id ultrices tellus, sed pretium ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+        featured: "yes",
+        "Technical-Specifications": { Size: "16 mm", Pressure: "Low Pressure" }
+    },    {
+        Name: "Product-3",
+        Brand: "Brand-2",
+        Images: ["https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder",],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur laoreet fermentum diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce congue leo in dolor tempus, a accumsan felis commodo. Fusce id ultrices tellus, sed pretium ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+        featured: "no",
+        "Technical-Specifications": { Size: "16 mm", Pressure: "Low Pressure" }
+    },    {
+        Name: "Product-4",
+        Brand: "Brand-1",
+        Images: ["https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder",],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur laoreet fermentum diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce congue leo in dolor tempus, a accumsan felis commodo. Fusce id ultrices tellus, sed pretium ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+        featured: "yes",
+        "Technical-Specifications": { Size: "16 mm", Pressure: "Low Pressure" }
+    },    {
+        Name: "Product-5",
+        Brand: "Al Fanar",
+        Images: ["https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder",],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur laoreet fermentum diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce congue leo in dolor tempus, a accumsan felis commodo. Fusce id ultrices tellus, sed pretium ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+        featured: "yes",
+        "Technical-Specifications": { Size: "16 mm", Pressure: "Low Pressure" }
+    },    {
+        Name: "Product-6",
+        Brand: "Al Fanar",
+        Images: ["https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder",],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur laoreet fermentum diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce congue leo in dolor tempus, a accumsan felis commodo. Fusce id ultrices tellus, sed pretium ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+        featured: "no",
+        "Technical-Specifications": { Size: "16 mm", Pressure: "Low Pressure" }
+    },    {
+        Name: "Product-7",
+        Brand: "Brand-3",
+        Images: ["https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder",],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur laoreet fermentum diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce congue leo in dolor tempus, a accumsan felis commodo. Fusce id ultrices tellus, sed pretium ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+        featured: "no",
+        "Technical-Specifications": { Size: "16 mm", Pressure: "Low Pressure" }
+    },    {
+        Name: "Product-8",
+        Brand: "Brand-3",
+        Images: ["https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder",],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur laoreet fermentum diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce congue leo in dolor tempus, a accumsan felis commodo. Fusce id ultrices tellus, sed pretium ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+        featured: "no",
+        "Technical-Specifications": { Size: "16 mm", Pressure: "Low Pressure" }
+    },    {
+        Name: "Product-9",
+        Brand: "Brand-1",
+        Images: ["https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder",],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur laoreet fermentum diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce congue leo in dolor tempus, a accumsan felis commodo. Fusce id ultrices tellus, sed pretium ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+        featured: "yes",
+        "Technical-Specifications": { Size: "16 mm", Pressure: "Low Pressure" }
+    },    {
+        Name: "Product-19",
+        Brand: "Al Fanar",
+        Images: ["https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder",],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur laoreet fermentum diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce congue leo in dolor tempus, a accumsan felis commodo. Fusce id ultrices tellus, sed pretium ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+        featured: "yes",
+        "Technical-Specifications": { Size: "16 mm", Pressure: "Low Pressure" }
+    },    {
+        Name: "Product-1",
+        Brand: "Brand-1",
+        Images: ["https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder", "https://via.placeholder.com/250?text=Placeholder",],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur laoreet fermentum diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce congue leo in dolor tempus, a accumsan felis commodo. Fusce id ultrices tellus, sed pretium ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+        featured: "no",
+        "Technical-Specifications": { Size: "16 mm", Pressure: "Low Pressure" }
+    }
+]
