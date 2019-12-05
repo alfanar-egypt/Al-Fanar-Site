cfd3e7fd7af15f875ccd9a63dd2b1c416fc8c935
Ahmed Khalil <ahmed8157@gmail.com>
2019-12-05 22:25:43 +0200

    add meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { TranslateService } from "@ngx-translate/core";
+import { Meta } from '@angular/platform-browser';
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -8,7 +9,9 @@ import { TranslateService } from "@ngx-translate/core";
 export class AppComponent {
   lang;
   ltrrtl;
-  constructor(public translate: TranslateService) {
+  constructor(public translate: TranslateService, private meta: Meta) {
+    this.meta.addTag({ name: 'description', content: 'Al Fanar Medical Center was established on 1996, specialized in the field of orthopedics and neurosurgery supplies. Our mission is to provide the best quality to the patients, the most reliable and professional services,' });
+    this.meta.addTag({ name: 'keywords', content: 'Fanar, Orthopedics, Neurosurgery' });
     translate.addLangs(['en', 'ar']);
     translate.setDefaultLang('en');
     const browserLang = translate.getBrowserLang();
