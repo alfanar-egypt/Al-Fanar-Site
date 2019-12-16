574b5bf9931f4848ff37ab8d153d4f59ffa38271
Ahmed Khalil <ahmed8157@gmail.com>
2019-12-16 11:27:21 +0200

    make the year in the footer dynamic

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+oggi = new Date();
+year = this.oggi.getFullYear()
   constructor() {}
 
   ngOnInit() {}
